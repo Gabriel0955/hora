8db8516c700e711790614d3825827259821674ed
Gabriel Aguirre <108961289+Gabriel0955@users.noreply.github.com>
2023-10-31 16:06:19 -0500

    Add files via upload

--- estilos/js.js
+++ estilos/js.js
@@ -1,11 +1,15 @@
 let iron = document.getElementById("iron")
 let mon = document.getElementById("mon")
 function sitio(){
-    iron.style.marginTop = "-300px"
-    iron.style.transition = "4s"
-    mon.style.marginTop = "-300px"
-    mon.style.transition = "4s"
+    iron.style.animation = "anima 10s infinite"
+    mon.style.animation = "anima 10s infinite"
+    
+    
     
 
 }
-iron.addEventListener("click",sitio);mon.addEventListener("click",sitio)
+function detenerAnimacion() {
+    iron.style.animation = '';
+}
+
+sitio();
