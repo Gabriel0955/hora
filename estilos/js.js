516a0dbedcf4fcb62e81f210ad731a0b1a85106f
Gabriel Aguirre <108961289+Gabriel0955@users.noreply.github.com>
2023-11-05 14:41:22 -0500

    Add files via upload

--- estilos/js.js
+++ estilos/js.js
@@ -1,15 +1,23 @@
 let iron = document.getElementById("iron")
 let mon = document.getElementById("mon")
+let irondes = document.getElementById("irondes")
+let mondes = document.getElementById("mondes")
 function sitio(){
     iron.style.animation = "anima 10s infinite"
     mon.style.animation = "anima 10s infinite"
+    iron.style.visibility = "visible"
+    mon.style.visibility = "visible"
+    irondes.style.visibility = "hidden"
+    mondes.style.visibility = "hidden"
     
     
     
 
 }
 function detenerAnimacion() {
-    iron.style.animation = '';
+    iron.style.visibility = 'hidden';
+    mon.style.visibility = 'hidden';
 }
 
-sitio();
+setTimeout("detenerAnimacion()",0)
+setTimeout("sitio()",3000)
